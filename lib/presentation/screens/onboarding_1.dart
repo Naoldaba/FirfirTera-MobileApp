9b2efd69c432f10b6c2857a5ef6572a9a5bd4897
Eyob Derese <eyobderese@gmail.com>
2024-04-17 20:50:03 +0300

    Adding onboarding I

---
+++ lib/presentation/screens/onboarding_1.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'dart:async';
+import 'package:firfir_tera/presentation/widgets/brand_promo.dart';
+
+class OnBoarding_1 extends StatefulWidget {
+  const OnBoarding_1({super.key});
+
+  @override
+  State<OnBoarding_1> createState() => _OnBoarding_1State();
+}
+
+class _OnBoarding_1State extends State<OnBoarding_1> {
+  @override
+  void initState() {
+    super.initState();
+    Timer(const Duration(seconds: 5), () {
+      Navigator.pushReplacementNamed(context, "/onboarding_2");
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Padding(
+            padding: EdgeInsets.symmetric(horizontal: 20),
+            child: BrandPromo(
+              color: Colors.black,
+            )),
+      ),
+    );
+  }
+}
