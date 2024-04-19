e629a15b76edff2fd1dd05e3b9eb6d41aba2d13a
NaolDaba <nahafile@gmail.com>
2024-04-20 00:04:14 +0300

    added the profile screen

---
+++ lib/presentation/screens/profile.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+import 'edit_profile.dart';
+
+class Profile extends StatelessWidget {
+  const Profile({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              const CircleAvatar(
+                radius: 80,
+                backgroundImage: AssetImage('assets/profile_pic/profile_1.jpg'),
+              ),
+              const SizedBox(height: 20),
+              const Text(
+                'Naol Daba',
+                style: TextStyle(fontSize: 28, fontWeight: FontWeight.bold),
+              ),
+              const SizedBox(height: 10),
+              const Text(
+                'naoldaba@gmail.com',
+                style: TextStyle(fontSize: 20, color: Colors.grey),
+              ),
+              const SizedBox(height: 30),
+              ElevatedButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => const EditProfile()),
+                  );
+                },
+                style: ButtonStyle(
+                    backgroundColor: MaterialStateProperty.all(Colors.orange),
+                    minimumSize: MaterialStateProperty.all(const Size(90, 40)),
+                    shape: MaterialStateProperty.all(RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(40)))),
+                child: const Text('Edit Profile'),
+              ),
+              const SizedBox(height: 20),
+              OutlinedButton(
+                onPressed: () {},
+                style: ButtonStyle(
+                    minimumSize: MaterialStateProperty.all(const Size(90, 40)),
+                    shape: MaterialStateProperty.all(RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(40)))),
+                child: const Text('Log Out', style: TextStyle(color: Colors.red)),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
