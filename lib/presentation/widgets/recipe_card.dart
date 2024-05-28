286bb0e7a7e7fe054e1c07d673d0142eb95169a2
NaolDaba <nahafile@gmail.com>
2024-05-28 23:29:42 +0300

    modified the recipe card widget

--- lib/presentation/widgets/recipe_card.dart
+++ lib/presentation/widgets/recipe_card.dart
@@ -1,28 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:firfir_tera/models/Recipe.dart';
 
-class RecipeCard extends StatefulWidget {
-  final String imagePath;
-  final String recipeName;
+class RecipeCard extends StatelessWidget {
+  final String image;
+  final String name;
   final double width;
   final double height;
 
   const RecipeCard({
-    required this.imagePath,
-    required this.recipeName,
+    required this.image,
+    required this.name,
     this.width = 200,
     this.height = 250,
     Key? key,
   }) : super(key: key);
 
   @override
-  State<RecipeCard> createState() => _RecipeCardState();
-}
-
-class _RecipeCardState extends State<RecipeCard> {
-  @override
   Widget build(BuildContext context) {
     return SizedBox(
-      width: widget.width,
+      width: width,
       height: 100,
       child: Card(
         elevation: 4,
@@ -30,9 +26,6 @@ class _RecipeCardState extends State<RecipeCard> {
           borderRadius: BorderRadius.circular(12.0),
         ),
         child: InkWell(
-          onTap: () {
-            Navigator.pushNamed(context, '/home/detailed_view');
-          },
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.stretch,
             children: [
@@ -40,15 +33,15 @@ class _RecipeCardState extends State<RecipeCard> {
                 borderRadius:
                     const BorderRadius.vertical(top: Radius.circular(12.0)),
                 child: Image.asset(
-                  widget.imagePath,
-                  height: widget.height * 0.9,
+                  image,
+                  height: height * 0.9,
                   fit: BoxFit.cover,
                 ),
               ),
               Padding(
                 padding: const EdgeInsets.all(2.0),
                 child: Text(
-                  widget.recipeName,
+                  name,
                   style: const TextStyle(
                     fontSize: 18,
                     fontWeight: FontWeight.bold,
@@ -64,52 +57,51 @@ class _RecipeCardState extends State<RecipeCard> {
   }
 }
 
-class Recipe {
-  final String imagePath;
-  final String recipeName;
+// class Recipe {
+//   final String imagePath;
+//   final String recipeName;
 
-  Recipe({
-    required this.imagePath,
-    required this.recipeName,
-  });
-}
+//   Recipe({
+//     required this.imagePath,
+//     required this.recipeName,
+//   });
+// }
 
-List<Recipe> recipeList = [
-  Recipe(
-    imagePath: 'assets/images/kikil.jpg',
-    recipeName: 'Kikil',
-  ),
-  Recipe(
-    imagePath: 'assets/images/tibs.jpg',
-    recipeName: 'Tibs',
-  ),
-  Recipe(
-    imagePath: 'assets/images/beyaynet_fisik.jpg',
-    recipeName: "Yefisik Beyaynet",
-  ),
-  Recipe(
-    imagePath: 'assets/images/shiro.webp',
-    recipeName: "Shiro",
-  ),
-  Recipe(
-    imagePath: 'assets/images/pasta.jpg',
-    recipeName: "Pasta",
-  ),
-  Recipe(
-    imagePath: 'assets/images/beyaynet_tsom.jpg',
-    recipeName: "Yetsom Beyaynet",
-  ),
-  Recipe(
-    imagePath: 'assets/images/firfir.jpg',
-    recipeName: "Firfir",
-  ),
-  Recipe(
-    imagePath: 'assets/images/Tegabino.png',
-    recipeName: "Tegabino",
-  ),
-  Recipe(
-    imagePath: 'assets/images/sambusa.jpg',
-    recipeName: "Sambusa",
-  ),
-  
-];
+// List<Recipe> recipeList = [
+  // Recipe(
+  //   imagePath: 'assets/images/kikil.jpg',
+  //   recipeName: 'Kikil',
+  // ),
+//   Recipe(
+//     imagePath: 'assets/images/tibs.jpg',
+//     recipeName: 'Tibs',
+//   ),
+//   Recipe(
+//     imagePath: 'assets/images/beyaynet_fisik.jpg',
+//     recipeName: "Yefisik Beyaynet",
+//   ),
+//   Recipe(
+//     imagePath: 'assets/images/shiro.webp',
+//     recipeName: "Shiro",
+//   ),
+//   Recipe(
+//     imagePath: 'assets/images/pasta.jpg',
+//     recipeName: "Pasta",
+//   ),
+//   Recipe(
+//     imagePath: 'assets/images/beyaynet_tsom.jpg',
+//     recipeName: "Yetsom Beyaynet",
+//   ),
+//   Recipe(
+//     imagePath: 'assets/images/firfir.jpg',
+//     recipeName: "Firfir",
+//   ),
+//   Recipe(
+//     imagePath: 'assets/images/Tegabino.png',
+//     recipeName: "Tegabino",
+//   ),
+//   Recipe(
+//     imagePath: 'assets/images/sambusa.jpg',
+//     recipeName: "Sambusa",
+//   ),
+// ];
