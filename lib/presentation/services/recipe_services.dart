b8255948bc84d363f79efc67048a0c5e756d756f
NaolDaba <nahafile@gmail.com>
2024-05-31 04:00:04 +0300

    made changes to the recipes sercive screen

--- lib/presentation/services/recipe_services.dart
+++ lib/presentation/services/recipe_services.dart
@@ -4,128 +4,203 @@ import 'package:http/http.dart' as http;
 import 'package:flutter/material.dart';
 import 'package:path/path.dart';
 import 'package:http_parser/http_parser.dart';
+import 'package:firfir_tera/models/Recipe.dart';
 
-Future<bool> DeleteRecipe(String id) async {
-  final response = await http.delete(Uri.parse(''));
-  if (response.statusCode == 204) {
-    print('Recipe deleted');
-    return true;
-  } else {
-    print('Failed to delete recipe. Status code: ${response.statusCode}');
-    return false;
+const String url = 'https://ee64-196-189-150-186.ngrok-free.app';
+
+class RecipeServices {
+  Future<List<Recipe>> recipes() async {
+    final response = await http.get(Uri.parse("$url/recipes"));
+    try {
+      if (response.statusCode == 200) {
+        final data = jsonDecode(response.body) as List;
+        return data
+            .map((recipe) => Recipe.fromJson(recipe))
+            .where((recipe) => recipe.type == 'breakfast')
+            .toList();
+      } else {
+        throw Exception("");
+      }
+    } catch (e) {
+      throw Exception();
+    }
   }
-}
 
-Future<void> sendPatchRequest({
-  required BuildContext context,
-  required String name,
-  required String description,
-  required String cookTime,
-  required String people,
-  required String type,
-  required File image,
-  required List<String> ingredients,
-  required List<String> steps,
-}) async {
-  if (name.isNotEmpty &&
-      description.isNotEmpty &&
-      cookTime.isNotEmpty &&
-      people.isNotEmpty &&
-      type.isNotEmpty &&
-      ingredients.isNotEmpty &&
-      steps.isNotEmpty) {
-    final url = Uri.parse('');
-    final request = http.MultipartRequest('PATCH', url);
+  Future<List<Recipe>> breakfastRecipes() async {
+    final response = await http.get(Uri.parse("$url/recipes"));
+    try {
+      if (response.statusCode == 200) {
+        final data = jsonDecode(response.body) as List;
+        return data
+            .map((recipe) => Recipe.fromJson(recipe))
+            .where((recipe) => recipe.type == 'lunch')
+            .toList();
+      } else {
+        throw Exception("");
+      }
+    } catch (e) {
+      rethrow;
+    }
+  }
 
-    request.fields['name'] = name;
-    request.fields['description'] = description;
-    request.fields['cookTime'] = cookTime;
-    request.fields['people'] = people;
-    request.fields['type'] = type;
-    request.fields['ingredients'] = jsonEncode(ingredients);
-    request.fields['steps'] = jsonEncode(steps);
+  Future<List<Recipe>> lunchRecipes() async {
+    final response = await http.get(Uri.parse("$url/recipes"));
+    try {
+      if (response.statusCode == 200) {
+        final data = jsonDecode(response.body) as List;
+        return data
+            .map((recipe) => Recipe.fromJson(recipe))
+            .where((recipe) => recipe.type == 'dinner')
+            .toList();
+      } else {
+        throw Exception("");
+      }
+    } catch (e) {
+      rethrow;
+    }
+  }
 
-    request.files.add(await http.MultipartFile.fromPath(
-      'image',
-      image.path,
-      filename: basename(image.path),
-      contentType: MediaType('image', 'jpeg'),
-    ));
+  Future<List<Recipe>> dinnerRecipes() async {
+    final response = await http.get(Uri.parse("$url/recipes"));
+    try {
+      if (response.statusCode == 200) {
+        final data = jsonDecode(response.body) as List;
+        return data
+            .map((recipe) => Recipe.fromJson(recipe))
+            .where((recipe) => recipe.type == '')
+            .toList();
+      } else {
+        throw Exception("");
+      }
+    } catch (e) {
+      rethrow;
+    }
+  }
 
-    final response = await request.send();
-    // print(request.fields);
-    // print(request.files);
+  Future<bool> DeleteRecipe(String id) async {
+    final response = await http.delete(Uri.parse('$url/recipes/${id}'));
+    if (response.statusCode == 204) {
+      print('Recipe deleted');
+      return true;
+    } else {
+      print('Failed to delete recipe. Status code: ${response.statusCode}');
+      return false;
+    }
+  }
 
-    // final response = true;
-    if (response.statusCode==200) {
-      ScaffoldMessenger.of(context).showSnackBar(
-        SnackBar(content: Text('Recipe updated successfully')),
-      );
+  Future<void> sendPatchRequest({
+    required BuildContext context,
+    required String id,
+    required String name,
+    required String description,
+    required String cookTime,
+    required String people,
+    required String type,
+    required File image,
+    required List<String> ingredients,
+    required List<String> steps,
+  }) async {
+    if (id.isNotEmpty &&
+        name.isNotEmpty &&
+        description.isNotEmpty &&
+        cookTime.isNotEmpty &&
+        people.isNotEmpty &&
+        type.isNotEmpty &&
+        ingredients.isNotEmpty &&
+        steps.isNotEmpty) {
+      final request =
+          http.MultipartRequest('PATCH', Uri.parse('$url/recipes/$id'));
+
+      request.fields['name'] = name;
+      request.fields['description'] = description;
+      request.fields['cookTime'] = cookTime;
+      request.fields['people'] = people;
+      request.fields['type'] = type;
+      request.fields['ingredients'] = jsonEncode(ingredients);
+      request.fields['steps'] = jsonEncode(steps);
+
+      request.files.add(await http.MultipartFile.fromPath(
+        'image',
+        image.path,
+        filename: basename(image.path),
+        contentType: MediaType('image', 'jpeg'),
+      ));
+
+      final response = await request.send();
+      // print(request.fields);
+      // print(request.files);
+
+      // final response = true;
+      if (response.statusCode == 200) {
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(content: Text('Recipe updated successfully')),
+        );
+      } else {
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(content: Text('Failed to update recipe')),
+        );
+      }
     } else {
       ScaffoldMessenger.of(context).showSnackBar(
-        SnackBar(content: Text('Failed to update recipe')),
+        SnackBar(content: Text('Please fill out all fields')),
       );
     }
-  } else {
-    ScaffoldMessenger.of(context).showSnackBar(
-      SnackBar(content: Text('Please fill out all fields')),
-    );
   }
-}
 
-Future<void> sendPostRequest({
-  required BuildContext context,
-  required String name,
-  required String description,
-  required String cookTime,
-  required String people,
-  required String type,
-  required File image,
-  required List<String> ingredients,
-  required List<String> steps,
-}) async {
-  if (name.isNotEmpty &&
-      description.isNotEmpty &&
-      cookTime.isNotEmpty &&
-      people.isNotEmpty &&
-      type.isNotEmpty &&
-      ingredients.isNotEmpty &&
-      steps.isNotEmpty) {
-    final url = Uri.parse('');
-    final request = http.MultipartRequest('POST', url);
+  Future<void> sendPostRequest({
+    required BuildContext context,
+    required String name,
+    required String description,
+    required String cookTime,
+    required String people,
+    required String type,
+    required File image,
+    required List<String> ingredients,
+    required List<String> steps,
+  }) async {
+    if (name.isNotEmpty &&
+        description.isNotEmpty &&
+        cookTime.isNotEmpty &&
+        people.isNotEmpty &&
+        type.isNotEmpty &&
+        ingredients.isNotEmpty &&
+        steps.isNotEmpty) {
+      final url = Uri.parse('');
+      final request = http.MultipartRequest('POST', url);
 
-    request.fields['name'] = name;
-    request.fields['description'] = description;
-    request.fields['cookTime'] = cookTime;
-    request.fields['people'] = people;
-    request.fields['type'] = type;
-    request.fields['ingredients'] = jsonEncode(ingredients);
-    request.fields['steps'] = jsonEncode(steps);
+      request.fields['name'] = name;
+      request.fields['description'] = description;
+      request.fields['cookTime'] = cookTime;
+      request.fields['people'] = people;
+      request.fields['type'] = type;
+      request.fields['ingredients'] = jsonEncode(ingredients);
+      request.fields['steps'] = jsonEncode(steps);
 
-    request.files.add(await http.MultipartFile.fromPath(
-      'image',
-      image.path,
-      filename: basename(image.path),
-      contentType: MediaType('image', 'jpeg'),
-    ));
+      request.files.add(await http.MultipartFile.fromPath(
+        'image',
+        image.path,
+        filename: basename(image.path),
+        contentType: MediaType('image', 'jpeg'),
+      ));
 
-    final response = await request.send();
-    // print(request.fields);
-    // print(request.fields);
-    // final response = true;
+      final response = await request.send();
+      // print(request.fields);
+      // print(request.fields);
+      // final response = true;
 
-    if (response.statusCode==200) {
-      ScaffoldMessenger.of(context).showSnackBar(
-        SnackBar(content: Text('Recipe created successfully')),
-      );
+      if (response.statusCode == 200) {
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(content: Text('Recipe created successfully')),
+        );
+      } else {
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(content: Text('Failed to create recipe')),
+        );
+      }
     } else {
       ScaffoldMessenger.of(context).showSnackBar(
-        SnackBar(content: Text('Failed to create recipe')),
+        SnackBar(content: Text('Please fill the remaining fields')),
       );
     }
-  } else {
-    ScaffoldMessenger.of(context).showSnackBar(
-      SnackBar(content: Text('Please fill the remaining fields')),
-    );
   }
 }
