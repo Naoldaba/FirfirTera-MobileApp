ccc447a6583725e1af2939e7b8368c794fc8ba7a
NaolDaba <nahafile@gmail.com>
2024-05-31 03:55:27 +0300

    modified the comment screen

--- lib/models/Comment.dart
+++ lib/models/Comment.dart
@@ -1,11 +1,11 @@
 class Comment {
-  final String id;
+  final String? id;
   final String userId;
   final String comment;
   final String recipeId;
 
   Comment({
-    required this.id,
+    this.id,
     required this.recipeId,
     required this.userId,
     required this.comment,
@@ -22,18 +22,10 @@ class Comment {
 
   Map<String, dynamic> toJson() {
     return {
-      'id': id,
       'recipeId': recipeId,
       'userId': userId,
       'comment': comment
     };
   }
 
-  Comment copyWith({String? recipeId, String? userId, String? comment}) {
-    return Comment(
-        id: id ?? this.id,
-        recipeId: recipeId ?? this.recipeId,
-        userId: userId ?? this.userId,
-        comment: comment ?? this.comment);
-  }
 }
