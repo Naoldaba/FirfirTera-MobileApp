467d279f7708e5eec1aa1ffbc84959d6b1440dba
NaolDaba <nahafile@gmail.com>
2024-06-29 23:29:24 +0300

    getting the backend ready for deployment

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -7,21 +7,33 @@ import { join } from 'path';
 const path = require('path');
 
 async function bootstrap() {
+  console.log('Creating Nest application...');
   const app = await NestFactory.create(AppModule);
+
+  console.log('Enabling CORS...');
   app.enableCors({
     origin: '*',
     methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
   });
+
+  console.log('Setting up global pipes...');
   app.useGlobalPipes(new ValidationPipe());
+
+  console.log('Setting up static file serving...');
   app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
   app.use(express.json({ limit: '10mb' }));
 
+  console.log('Setting up root route...');
   app.use('/', (req, res) => {
     res.send('Welcome');
   });
 
-  await app.listen(3000).then(()=>{console.log("server running on port ")});
+  console.log('Starting the application...');
+  await app.listen(3000);
+  console.log('Application started.');
+
   return app.getHttpAdapter().getInstance();
 }
 
+console.log('Bootstrap process starting...');
 export default bootstrap();
