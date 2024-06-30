b6f559a708542099b4e19b9dcce36e3de6050e5f
NaolDaba <nahafile@gmail.com>
2024-06-30 16:26:49 +0300

    made modifications

--- backend/src/recipe/recipe.controller.ts
+++ backend/src/recipe/recipe.controller.ts
@@ -68,7 +68,7 @@ export class RecipeController {
       file.path,
     );
 
-    const serverBaseURL = 'https://firfir-tera-backend.vercel.app';
+    const serverBaseURL = 'https://firfir-tera-backend.vercel.app/uploads/';
     const filePath = `${serverBaseURL}${file.filename}`;
     const createdRecipe = await this.recipeService.insertRecipe(
       {
@@ -139,7 +139,7 @@ export class RecipeController {
   ) {
 
       this.uploadService.uploadFile(file) 
-      const serverBaseURL = 'https://firfir-tera-backend.vercel.app';
+      const serverBaseURL = 'https://firfir-tera-backend.vercel.app/uploads/';
       const image = `${serverBaseURL}${file.filename}`;
       console.log('the Id is:', recipeId);
       console.log('thumbs up')
